9e98b45ff5fb77079ba9bc2e8e20fe9a0682a8dd
Kelvin Chan <kelvinc.25@gmail.com>
2017-11-25 16:55:07 -0500

    Add event loop to run multiple clients

---
+++ client.py
@@ -0,0 +1,39 @@
+import discord
+import asyncio
+
+
+class BaseClient(discord.Client):
+    def __str__(self):
+        return '{0}'.format(self.__class__.__name__)
+
+    @asyncio.coroutine
+    def on_ready(self):
+        print('{0}: Logged in as'.format(self))
+        print(self.user.name)
+        print(self.user.id)
+        print('------')
+
+    @asyncio.coroutine
+    def on_message(self, message):
+        self.handle_message(message)
+
+    @asyncio.coroutine
+    def on_message_delete(self, message):
+        self.handle_message_delete(message)
+
+    def handle_message(self, message):
+        print('{0}: Received message - {1}'.format(message, self))
+
+    def handle_message_delete(self, message):
+        pass
+
+
+class ListenerClient(BaseClient):
+    def __init__(self, relay_client=None, *args, **kwargs):
+        self.relay_client = relay_client
+        super(ListenerClient, self).__init__(*args, **kwargs)
+
+
+class RelayClient(BaseClient):
+    def handle_message(self, message):
+        pass
--- main.py
+++ main.py
@@ -1,76 +1,62 @@
-import discord
 import asyncio
+from collections import namedtuple
 import os
 
-
-class BaseClient(discord.Client):
-    def __str__(self):
-        return '{0}'.format(self.__class__.__name__)
-
-    @asyncio.coroutine
-    def on_ready(self):
-        print('{0}: Logged in as'.format(self))
-        print(self.user.name)
-        print(self.user.id)
-        print('------')
-
-    @asyncio.coroutine
-    def on_message(self, message):
-        self.handle_message(message)
-
-    @asyncio.coroutine
-    def on_message_delete(self, message):
-        self.handle_message_delete(message)
-
-    def handle_message(self, message):
-        print('{0}: Received message - {1}'.format(message, self))
-
-    def handle_message_delete(self, message):
-        pass
-
-
-class ListenerClient(BaseClient):
-    def __init__(self, relay_client=None, *args, **kwargs):
-        self.relay_client = relay_client
-        super(ListenerClient, self).__init__(*args, **kwargs)
-
-
-class RelayClient(BaseClient):
-    def handle_message(self, message):
-        pass
-
-
-#
-#
-# @listenerClient.event
-# async def on_message(message):
-#     if message.content.startswith('!test'):
-#         counter = 0
-#         tmp = await listenerClient.send_message(message.channel, 'Calculating messages...')
-#         async for log in listenerClient.logs_from(message.channel, limit=100):
-#             if log.author == message.author:
-#                 counter += 1
-#
-#         await listenerClient.edit_message(tmp, 'You have {} messages.'.format(counter))
-#     elif message.content.startswith('!sleep'):
-#         await asyncio.sleep(5)
-#         await listenerClient.send_message(message.channel, 'Done sleeping')
-#
-#
-# @relayClient.event
-# async def on_ready():
-#     print('Logged in as')
-#     print(relayClient.user.name)
-#     print(relayClient.user.id)
-#     print('------')
+from client import ListenerClient, RelayClient
 
 
 relayClient = RelayClient()
 listenerClient = ListenerClient(relayClient)
 
+
 if os.environ.get('RELAY_DEV_MODE', False):
-    relayClient.run(os.environ['DISCORD_DEV_BOT_TOKEN'])
+    RELAY_TOKEN_VAR = 'DISCORD_DEV_BOT_TOKEN'
 else:
-    relayClient.run(os.environ['DISCORD_BOT_TOKEN'])
+    RELAY_TOKEN_VAR = 'DISCORD_BOT_TOKEN'
 
 listenerClient.run(os.environ['BDO_BOSS_TRACKER_LISTENER_TOKEN'])
+
+
+# First, we must attach an event signalling when the bot has been
+# closed to the client itself so we know when to fully close the event loop.
+
+Entry = namedtuple('Entry', 'client event token')
+entries = [
+    Entry(client=relayClient, event=asyncio.Event(), token=os.environ[RELAY_TOKEN_VAR]),
+    Entry(client=listenerClient, event=asyncio.Event(), token=os.environ['BDO_BOSS_TRACKER_LISTENER_TOKEN'])
+]
+
+# Then, we should login to all our clients and wrap the connect call
+# so it knows when to do the actual full closure
+
+loop = asyncio.get_event_loop()
+
+async def login():
+    for e in entries:
+        await e.client.login(e.token)
+
+async def wrapped_connect(entry):
+    try:
+        await entry.client.connect()
+    except Exception as e:
+        await entry.client.close()
+        print('We got an exception: ', e.__class__.__name__, e)
+        entry.event.set()
+
+# actually check if we should close the event loop:
+async def check_close():
+    futures = [e.event.wait() for e in entries]
+    await asyncio.wait(futures)
+
+# here is when we actually login
+loop.run_until_complete(login())
+
+# now we connect to every client
+for entry in entries:
+    loop.create_task(wrapped_connect(entry))
+
+# now we're waiting for all the clients to close
+loop.run_until_complete(check_close())
+
+# finally, we close the event loop
+loop.close()
